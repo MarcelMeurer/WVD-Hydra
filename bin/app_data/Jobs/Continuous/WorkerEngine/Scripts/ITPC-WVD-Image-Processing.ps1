22ed8c108232edc7e6a29cab5721ae5aba33745d
Marcel Meurer <Marcel.Meurer@itprocloud.de>
2023-01-20 14:42:39 +0100

    Update

--- bin/app_data/Jobs/Continuous/WorkerEngine/Scripts/ITPC-WVD-Image-Processing.ps1
+++ bin/app_data/Jobs/Continuous/WorkerEngine/Scripts/ITPC-WVD-Image-Processing.ps1
@@ -519,11 +519,14 @@ if ($mode -eq "Generalize") {
 	if ($modifyDrives -eq $false)
 	{
 		try {
+			$defragSvc=Get-Service -Name defragsvc -ErrorAction SilentlyContinue
+			Set-Service -Name defragsvc -StartupType Manual -ErrorAction SilentlyContinue
 			$supportedSize = (Get-PartitionSupportedSize -DriveLetter "c" -ErrorAction Stop)
 			if ((Get-Partition -DriveLetter "c").Size -lt $supportedSize.SizeMax) {
 				LogWriter("Resize C: partition to fill up the disk")
 				Resize-Partition -DriveLetter "c" -Size $supportedSize.SizeMax
 			}
+			Set-Service -Name defragsvc -StartupType $defragSvc.StartType -ErrorAction SilentlyContinue
 		} catch {
 			LogWriter("Resize C: partition failed: $_")
 		}
